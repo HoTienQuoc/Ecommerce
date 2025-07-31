f21a57c9cda17cb036d5bd89d62c864afb78cd6d
HoTienQuoc <hotienquoc0429@gmail.com>
2025-07-31 20:25:22 +0700

    Add Product and ProductCategory entities with JPA annotations

--- demo/src/main/java/com/ecommerce/demo/entity/Product.java
+++ demo/src/main/java/com/ecommerce/demo/entity/Product.java
@@ -1,5 +1,61 @@
 package com.ecommerce.demo.entity;
 
-public class Product {
+import java.math.BigDecimal;
+import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.ManyToAny;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="Product")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="category_id", nullable = false)
+    private ProductCategory category;
+
+    @Column(name="sku")
+    private String sku;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="unitPrice")
+    private BigDecimal unitPrice;
+
+    @Column(name="imageUrl")
+    private String imageUrl;
+
+    @Column(name="active")
+    private Boolean active;
+
+    @Column(name="unitsInStock")
+    private Integer unitsInStock;
+
+    @Column(name="date_created")
+    @CreationTimestamp
+    private Date date_created;
+
+    @Column(name="last_updated")
+    @UpdateTimestamp
+    private Date last_updated;
 }
