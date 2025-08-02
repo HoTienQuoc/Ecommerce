e32310c6e5b51ab518a020eab8bc36a138d00930
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-02 21:04:10 +0700

    Remove ProductList component import from app.ts to streamline component dependencies

--- frontend/angular-ecommerce/src/app/app.ts
+++ frontend/angular-ecommerce/src/app/app.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ProductList } from "./components/product-list/product-list";
 import { ProductService } from './services/product.service';
 import { RouterModule } from '@angular/router';
 import { ProductCategoryMenu } from "./components/product-category-menu/product-category-menu";
@@ -7,7 +6,7 @@ import { Search } from "./components/search/search";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, ProductList, ProductCategoryMenu, Search],
+  imports: [RouterModule, ProductCategoryMenu, Search],
   providers:[ProductService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
