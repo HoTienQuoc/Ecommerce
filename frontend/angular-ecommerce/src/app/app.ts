496bbaa3850e6df8c2ec30b14c1f72b6fb98368b
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-05 21:53:00 +0700

    Refactor app layout to replace cart area with cart status component
    - Removed the static cart area from app.html and integrated the new app-cart-status component for improved cart visibility.
    - Updated app.ts to include the CartStatus component in the imports for proper functionality.

--- frontend/angular-ecommerce/src/app/app.ts
+++ frontend/angular-ecommerce/src/app/app.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 import { ProductCategoryMenu } from "./components/product-category-menu/product-category-menu";
 import { Search } from "./components/search/search";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatus } from "./components/cart-status/cart-status";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule, ProductCategoryMenu, Search, NgbModule],
+  imports: [RouterModule, ProductCategoryMenu, Search, NgbModule, CartStatus],
   providers:[ProductService],
   templateUrl: './app.html',
   styleUrl: './app.scss'
