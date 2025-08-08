7f573784cc0cf05ebad7d771721a9cdca1d302c7
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-08 22:32:00 +0700

    Implement add to cart and product description features in ProductDetails component
    - Added "Add to Cart" button to the product details view, triggering the addToCart method.
    - Included product description section for better product information display.
    - Updated HTML structure for improved layout and user experience.

--- frontend/angular-ecommerce/src/app/components/product-details/product-details.ts
+++ frontend/angular-ecommerce/src/app/components/product-details/product-details.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../common/product';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
 
 @Component({
   selector: 'app-product-details',
@@ -11,13 +13,16 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product-details.scss'
 })
 export class ProductDetails implements OnInit{
+
   product!: Product;
   constructor(
     private productService: ProductService,
+    private cartService:CartService,
     private route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
+    
     this.route.paramMap.subscribe(()=>{
       this.handleProductDetails();
     })
@@ -25,11 +30,22 @@ export class ProductDetails implements OnInit{
 
   handleProductDetails() {
     //get the "id" param string. convert string to a number using + symbol
+
+    // Chi tiết:
+    //   this.route.snapshot.paramMap.get('id')!: Lấy id từ URL (ví dụ: /products/3 → id = 3).
+    //   +: Ép kiểu từ string sang number.
+    //   Gọi productService.getProduct(theProductId) để fetch dữ liệu sản phẩm.
+    //   Gán kết quả trả về (data) vào biến this.product.
     const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
     
     this.productService.getProduct(theProductId).subscribe(
       data => {this.product = data}
     );
 
+  }
+
+  addToCart() {
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
   }
 }
