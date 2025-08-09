f88bf8d84b348f72e037a2e94a215780856711f1
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-09 11:41:00 +0700

    Add CartDetails route and update CartStatus link
    - Introduced a new route for CartDetails component to enhance cart navigation.
    - Updated the CartStatus component's link to use Angular's routeLink for proper routing to the cart details page.

--- frontend/angular-ecommerce/src/app/app.routes.ts
+++ frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { ProductDetails } from './components/product-details/product-details';
+import { CartDetails } from './components/cart-details/cart-details';
 
 export const routes: Routes = [
     {path: 'search/:keyword', component: ProductList},
     {path: 'products/:id', component: ProductDetails},
+    {path: 'cart-details', component: CartDetails},
     {path: 'category/:id', component: ProductList},
     {path: 'category', component: ProductList},
     {path: 'products', component: ProductList},
---
+++ frontend/angular-ecommerce/src/app/components/cart-details/cart-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-details',
+  imports: [],
+  templateUrl: './cart-details.html',
+  styleUrl: './cart-details.scss'
+})
+export class CartDetails {
+
+}
