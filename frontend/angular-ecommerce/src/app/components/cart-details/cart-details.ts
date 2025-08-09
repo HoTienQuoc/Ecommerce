161bc54bf3ef4017722f22036076ee7965d30450
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-09 12:04:44 +0700

    Implement CartDetails component with cart item listing and total calculations
    - Added CartService and CartItem imports to manage cart data.
    - Implemented OnInit lifecycle hook to initialize cart details on component load.
    - Created methods to list cart items and subscribe to total price and quantity updates from CartService.
    - Enhanced cart management functionality by computing totals directly within the CartDetails component.

--- frontend/angular-ecommerce/src/app/components/cart-details/cart-details.ts
+++ frontend/angular-ecommerce/src/app/components/cart-details/cart-details.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CartService } from './../../services/cart.service';
+import { CartItem } from './../../common/cart-item';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-details',
@@ -6,6 +8,28 @@ import { Component } from '@angular/core';
   templateUrl: './cart-details.html',
   styleUrl: './cart-details.scss'
 })
-export class CartDetails {
+export class CartDetails implements OnInit{
+  cartItem: CartItem[] = [];
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  constructor(private cartService: CartService){}
 
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+  listCartDetails() {
+    //get a handle to the cart items
+    this.cartItem = this.cartService.cartItems;
+    //subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    );
+    //subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      data=> this.totalQuantity = data
+    );
+    //compute cart total price and quantity
+    this.cartService.computeCartTotals();
+  }
 }
