1ccb9cd6ec7d567810d4fb72a3eed2174d951b89
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-14 16:41:46 +0700

    Enhance checkout form with improved credit card input handling
    - Added validation messages for credit card number and CVV fields, improving user feedback during input.
    - Introduced additional checks for credit card type selection, ensuring valid options are presented to users.
    - Updated Checkout component to streamline credit card validation logic, enhancing code maintainability.

---
+++ demo/src/main/java/com/ecommerce/demo/entity/Customer.java
@@ -0,0 +1,30 @@
+package com.ecommerce.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="Customer")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+}
