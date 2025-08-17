856ca57a07d603b65b89c08e6cc0114218916bc9
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-17 13:02:34 +0700

    Implement checkout functionality with order processing
    - Integrated CheckoutService to handle order placement via REST API.
    - Enhanced Checkout component to create and submit order and purchase details, including customer and shipping information.
    - Added methods to reset the cart and navigate back to the products page after order completion.
    - Improved order item handling by mapping cart items to order items efficiently.

--- frontend/angular-ecommerce/src/app/components/checkout/checkout.ts
+++ frontend/angular-ecommerce/src/app/components/checkout/checkout.ts
@@ -1,3 +1,4 @@
+import { CheckoutService } from './../../services/checkout.service';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -6,6 +7,10 @@ import { Country } from '../../common/country';
 import { State } from '../../common/state';
 import { Luv2ShopValidators } from '../../validators/luv2-shop-validators';
 import { CartService } from '../../services/cart.service';
+import { Router } from '@angular/router';
+import { Order } from '../../common/order';
+import { OrderItem } from '../../common/orderItem';
+import { Purchase } from '../../common/purchase';
 
 @Component({
   selector: 'app-checkout',
@@ -31,7 +36,9 @@ export class Checkout implements OnInit{
 
   constructor(private formBuilder: FormBuilder,
               private luv2ShopFormServiceService: Luv2ShopFormServiceService,
-              private cartService: CartService
+              private cartService: CartService,
+              private checkoutService: CheckoutService,
+              private router: Router,
   ){
 
   }
@@ -125,6 +132,73 @@ export class Checkout implements OnInit{
     if(this.checkoutFormGroup.invalid){
       this.checkoutFormGroup.markAllAsTouched();
     }
+    //set up order
+    let order = new Order();
+    order.totalPrice = this.totalPrice;
+    order.totalQuantity = this.totalQuantity;
+
+    //get cart items
+    const cartItems = this.cartService.cartItems;
+
+    //create orderItems from cartItems
+    //- long way
+    // let orderItems: OrderItem[] = [];
+    // for (let i = 0; i < cartItems.length; i++) {
+    //   orderItems[i] = new OrderItem(cartItems[i]);
+      
+    // }
+    // - short way of doing the same thingy
+    let orderItems: OrderItem[] = cartItems.map(tempCartItem => new OrderItem(tempCartItem));
+
+    //set up purchase
+    let purchase = new Purchase();
+
+    //populate purchase - customer
+    purchase.customer = this.checkoutFormGroup.controls['customer'].value;
+
+
+    //populate purchase - shipping address
+    purchase.shippingAddress = this.checkoutFormGroup.controls['shippingAddress'].value;
+    const shippingState: State = JSON.parse(JSON.stringify(purchase.shippingAddress.state)); 
+    const shippingCountry: Country = JSON.parse(JSON.stringify(purchase.shippingAddress.country));
+    purchase.shippingAddress.state = shippingState.name; 
+    purchase.shippingAddress.country = shippingCountry.name; 
+
+    //populate purchase - billing address
+    purchase.billingAddress = this.checkoutFormGroup.controls['billingAddress'].value;
+    const billingState: State = JSON.parse(JSON.stringify(purchase.billingAddress.state)); 
+    const billingCountry: Country = JSON.parse(JSON.stringify(purchase.billingAddress.country));
+    purchase.billingAddress.state = billingState.name; 
+    purchase.billingAddress.country = billingCountry.name; 
+
+    //populate purchase - order and orderItems
+    purchase.order = order;
+    purchase.orderItems = orderItems;
+
+    //call rest api via the CheckoutService 
+    this.checkoutService.placeOrder(purchase).subscribe({
+      //next: happy
+      next: response =>{
+        alert(`Your order has been received. \nOrder tracking number: ${response.orderTrackingNumber}`);
+        //reset cart
+        this.resetCart();
+      },
+      //error: exception
+      error: err => {
+        alert(`There was an error: ${err.message}`);
+      }
+    })
+
+  }
+  resetCart() {
+    //reset cart data
+    this.cartService.cartItems=[];
+    this.cartService.totalPrice.next(0);
+    this.cartService.totalQuantity.next(0);
+    //reset the form
+    this.checkoutFormGroup.reset();
+    //navigate back to the products page
+    this.router.navigateByUrl("/products");
   }
 
   reviewCartDetails() {
