f3cb16afc209991207c6291947e423dcb2ad1847
HoTienQuoc <hotienquoc0429@gmail.com>
2025-08-17 18:53:41 +0700

    Enhance routing and login status in Angular eCommerce app
    - Updated app.routes.ts to include a new MembersPage route with AuthGuard for authentication.
    - Added a LoginStatus component to display user authentication state and provide navigation options based on login status.
    - Modified login-status.html to improve user experience by showing the user's name when authenticated and providing a button to access member features.

--- frontend/angular-ecommerce/src/app/app.routes.ts
+++ frontend/angular-ecommerce/src/app/app.routes.ts
@@ -1,10 +1,20 @@
-import { Routes } from '@angular/router';
+import { Router, Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { ProductDetails } from './components/product-details/product-details';
 import { CartDetails } from './components/cart-details/cart-details';
 import { Checkout } from './components/checkout/checkout';
+import { LoginStatus } from './components/login-status/login-status';
+import { MembersPage } from './components/members-page/members-page';
+import { AuthGuard } from '@auth0/auth0-angular';
+import { Injector } from '@angular/core';
 
+function sendToLoginPage(injector: Injector){
+    const router = injector.get(Router);
+    router.navigate(["/login"]);
+}
 export const routes: Routes = [
+    {path: 'members', component: MembersPage, canActivate: [AuthGuard], data: {onAuthRequired: sendToLoginPage}},
+    {path: 'login', component: LoginStatus},
     {path: 'checkout', component: Checkout},
     {path: 'search/:keyword', component: ProductList},
     {path: 'products/:id', component: ProductDetails},
@@ -12,6 +22,6 @@ export const routes: Routes = [
     {path: 'category/:id', component: ProductList},
     {path: 'category', component: ProductList},
     {path: 'products', component: ProductList},
-    {path: '', redirectTo: '/products', pathMatch: 'full'},
-    {path: '**', redirectTo: '/products', pathMatch: 'full'},
+    {path: '', redirectTo: '/', pathMatch: 'full'},
+    {path: '**', redirectTo: '/', pathMatch: 'full'},
 ];
